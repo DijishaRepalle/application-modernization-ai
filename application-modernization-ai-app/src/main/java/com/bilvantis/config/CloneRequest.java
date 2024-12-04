31fa9391f1e67256a1685ab595f8cdecf40af32c
Dijisha Repalle <dijisha.repalle@bilvantis.io>
2024-12-04 11:41:04 +0530

    API for cloning the repository using token and repo url

---
+++ application-modernization-ai-app/src/main/java/com/bilvantis/config/CloneRequest.java
@@ -0,0 +1,15 @@
+package com.bilvantis.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CloneRequest {
+    private String repoUrl;
+
+    private String token;
+
+}
