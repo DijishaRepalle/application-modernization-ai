42d33356beb654b000b7df7b70fdcf2b3b1553b5
Dijisha Repalle <dijisha.repalle@bilvantis.io>
2024-12-06 12:56:04 +0530

    token expiration time change and code change

--- application-modernization-ai-service/src/main/java/com/bilvantis/serviceImpl/UserInformationServiceImpl.java
+++ application-modernization-ai-service/src/main/java/com/bilvantis/serviceImpl/UserInformationServiceImpl.java
@@ -2,15 +2,12 @@ package com.bilvantis.serviceImpl;
 
 import com.bilvantis.exception.ApplicationException;
 import com.bilvantis.exception.BadRequestException;
-import com.bilvantis.exception.DataNotFoundException;
-import com.bilvantis.exception.ProjectImplementationSaveFailedException;
 import com.bilvantis.model.UserInformation;
 import com.bilvantis.model.UserInformationDTO;
 import com.bilvantis.repository.UserInformationRepository;
 import com.bilvantis.service.UserInformationService;
 import com.bilvantis.util.AppModernizationProperties;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -21,10 +18,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static com.bilvantis.util.ModernizationAppConstants.ROLE;
-import static com.bilvantis.util.ProjectInformationServiceConstants.EXCEPTION_ERROR_MESSAGE;
-import static com.bilvantis.util.ProjectInformationServiceConstants.PROJECT_DETAILS_NOT_FOUND;
-import static com.bilvantis.util.UserInformationSupport.convertUserEntityTOUserDTO;
-import static com.bilvantis.util.UserInformationSupport.convertUsersDTOTOUsersEntity;
 
 @Service
 @Slf4j
@@ -39,19 +32,6 @@ public class UserInformationServiceImpl implements UserInformationService<UserIn
         this.userInformationRepository = userInformationRepository;
     }
 
-    @Override
-    public UserInformationDTO createProject(UserInformationDTO userInformationDTO) {
-        try {
-            if (ObjectUtils.isEmpty(userInformationDTO)) {
-                throw new DataNotFoundException(PROJECT_DETAILS_NOT_FOUND);
-            }
-            UserInformation saveProjectDetails = userInformationRepository.save(convertUsersDTOTOUsersEntity(userInformationDTO));
-            return convertUserEntityTOUserDTO(saveProjectDetails);
-        } catch (DataAccessException e) {
-            log.error(EXCEPTION_ERROR_MESSAGE, this.getClass().getSimpleName(), e.getStackTrace()[0].getMethodName());
-            throw new ProjectImplementationSaveFailedException(e.getMessage());
-        }
-    }
 
     @Override
     public Boolean getRoleBasedOnUserId(String userId) {
